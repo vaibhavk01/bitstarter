2ccc343dbceec9f3a429dd747f66e8cb8bb7593e
Ubuntu <ubuntu@ip-10-212-129-12.ec2.internal>
2013-07-13 20:57:15 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,10 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+
 
 app.get('/', function(request, response) {
   response.send('vaibhav');
