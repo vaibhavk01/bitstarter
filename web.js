3e1337772afb62d076a11080d6ee2128e10ae257
Ubuntu <ubuntu@ip-10-212-129-12.ec2.internal>
2013-07-14 11:10:28 +0000

    web.js

--- web.js
+++ web.js
@@ -1,22 +1,14 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-var fs = require('fs')
-fs.readFile('index.html', 'utf8', function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  
-});
+var http = require('http'),
+    fs = require('fs');
 
 
-app.get('/', function(request, response) {
-   response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(data);  
-        response.end();
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
