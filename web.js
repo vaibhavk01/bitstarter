df1738e8edb0191c87e2d03f6a7261865b01bf6d
Ubuntu <ubuntu@ip-10-212-129-12.ec2.internal>
2013-07-14 11:34:09 +0000

    orginital

--- web.js
+++ web.js
@@ -1,14 +1,20 @@
-var http = require('http'),
-    fs = require('fs');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+var fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  
+});
 
 
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+app.get('/', function(request, response) {
+  response.send('vaibhav'+data);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
